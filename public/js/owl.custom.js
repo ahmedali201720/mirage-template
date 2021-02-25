e4a7d55bd752d02ab97f65a4cbfa79b27328eb01
Ahmed Ali <ahmedalimohamed1994@gmail.com>
2021-02-25 17:32:42 +0200

    working on carousel

--- public/js/owl.custom.js
+++ public/js/owl.custom.js
@@ -1,19 +1,21 @@
-$(document).ready(function () {
+// $(document).ready(function () {
 
-    $(".banner-carousel").owlCarousel({
-        margin: 0,
-        loop: true,
-        mouseDrag: true,
-        touchDrag: true,
-        nav: true,
-        navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>'],
-        autoplay: false,
-        rtl: true,
-        responsive: {
-            0: {
-                items: 1
-            }
-        },
-        singleItem: true
-    });
-});
+//     $(".banner-carousel").owlCarousel({
+//         margin: 0,
+//         loop: true,
+//         mouseDrag: true,
+//         touchDrag: true,
+//         autoplay: true,
+//         autoplayTimeout: 3000,
+//         autoplayHoverPause: true,
+//         nav: true,
+//         navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+//         rtl: true,
+//         responsive: {
+//             1200: {
+//                 items: 1
+//             }
+//         },
+//         singleItem: true
+//     });
+// });
