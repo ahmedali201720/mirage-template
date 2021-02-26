91da1150d20f3c64284cdf2e4ec875aaf83f6366
Ahmed Ali <ahmedalimohamed1994@gmail.com>
2021-02-26 21:08:30 +0200

    adding loading page

--- public/js/layout.js
+++ public/js/layout.js
@@ -1,5 +1,9 @@
 $(document).ready(() => {
 
+    setTimeout(function () {
+        $("#loading-overlay").fadeOut(600);
+    }, 5000);
+
     setInterval(function () {
         $("#date-time-content .time").text(getTime());
         $("#date-time-content .date").text(getDate());
