02f4bf9b9bef31efdff544e68963bcfa13288c11
Ahmed Ali <ahmedalimohamed1994@gmail.com>
2021-02-26 22:09:11 +0200

    news ticker works

--- public/js/layout.js
+++ public/js/layout.js
@@ -22,4 +22,14 @@ $(document).ready(() => {
 
     // *******************************************************************************************
 
+    $('.news-ticker-item').AcmeTicker({
+        type: 'marquee',/*horizontal/horizontal/Marquee/type*/
+        direction: 'right',/*up/down/left/right*/
+        speed: 0.05,/*true/false/number*/ /*For vertical/horizontal 600*//*For marquee 0.05*//*For typewriter 50*/
+        autoplay: 2000,
+        controls: {
+            toggle: $('.acme-news-ticker-pause'),/*Can be used for horizontal/horizontal/typewriter*//*not work for marquee*/
+        }
+    });
+
 });
