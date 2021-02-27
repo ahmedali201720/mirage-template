46998f21ea50e6c81da847e7b6e554ef52777ff4
Ahmed Ali <ahmedalimohamed1994@gmail.com>
2021-02-27 20:11:28 +0200

    responsive stats done

--- public/js/features.js
+++ public/js/features.js
@@ -1,3 +1,23 @@
 /* Copy to Cipboard */
 new ClipboardJS('.clipboard-btn');
 new ClipboardJS('.btn');
+
+// ****************************************************************************************
+// Count up feature on scroll
+// ****************************************************************************************
+
+var statsTopOffset = $("#stats").offset().top;
+var countUpDone = false;
+
+$(window).scroll(() => {
+
+    if (!countUpDone && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
+        $(".counter").each(function (key, value) {
+            var counterElement = $(this);
+            var elementValue = parseInt(counterElement.text());
+            $(this).countup(elementValue);
+        })
+        countUpDone = true;
+    }
+
+})
