3e5fcc9513ac4e5c21ad6755d41470e22500c7d4
Ahmed Ali <ahmedalimohamed1994@gmail.com>
2021-02-28 02:44:36 +0200

    news section finished

--- public/js/owl.custom.js
+++ public/js/owl.custom.js
@@ -1,21 +1,48 @@
-// $(document).ready(function () {
+$(document).ready(function () {
 
-//     $(".banner-carousel").owlCarousel({
-//         margin: 0,
-//         loop: true,
-//         mouseDrag: true,
-//         touchDrag: true,
-//         autoplay: true,
-//         autoplayTimeout: 3000,
-//         autoplayHoverPause: true,
-//         nav: true,
-//         navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>'],
-//         rtl: true,
-//         responsive: {
-//             1200: {
-//                 items: 1
-//             }
-//         },
-//         singleItem: true
-//     });
-// });
+    $(".news-carousel").owlCarousel({
+        margin: 20,
+        loop: true,
+        mouseDrag: true,
+        touchDrag: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        nav: true,
+        navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+        rtl: true,
+        responsive: {
+            0: {
+                items: 1.2
+            },
+            280: {
+                items: 1
+            },
+            400: {
+                items: 1.2
+            },
+            576: {
+                items: 2.1
+            },
+            768: {
+                items: 2.75
+            },
+            992: {
+                items: 2.1
+            },
+            1200: {
+                items: 3
+            },
+            1300: {
+                items: 3.25
+            },
+            1500: {
+                items: 3.75
+            },
+            1600: {
+                items: 4.1
+            }
+        },
+    });
+
+});
